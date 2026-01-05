aedc9846b8fbfd11494cb57ebd3dda730bd845a2
Kedarisetti Poojitha <kpoojitha262@gmail.com>
2026-01-05 19:33:09 +0300

    Create decisionStat.py

---
+++ decisionStat.py
@@ -0,0 +1,39 @@
+#IF-ELIF-ELSE
+if 10>20:
+      printa(10)
+elif 10<20:
+       print(20)
+else:
+           print("this is else")
+           
+print()
+#NESTED IF
+if 2>1:
+    print("this is outer if ")
+    if 2>0:
+        print("this is inner if")
+    else:
+        print("this is inner else")
+else:
+    print("this is the outer else")
+print()
+
+#short-hand if
+if 5>3:  print("this is if")
+print()
+#Example of if elif else
+if 1>2 and 2>3:
+    print("move to else")
+elif 2<3 and 1<3:
+    print("3 is largest")
+else:
+    print("no output")
+
+print()
+#write a program person is eligible to vote or not 
+age=15
+if age>18:
+  print("Eligible to vote")
+else:
+      print("Not eligible to vote.")
+print()
