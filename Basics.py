5270ada7e4324d59948fd93db263b74aee6d84dc
Kedarisetti Poojitha <kpoojitha262@gmail.com>
2026-05-03 21:23:04 +0530

    Add Basics.py

---
+++ Basics.py
@@ -0,0 +1,38 @@
+#Variables
+a=10
+print(a)
+
+
+a=b=c=d=100
+print(c)
+
+
+a=1,2,3
+print(a)
+
+a,b,c,d=1,2,3,4
+print(a,b,c,d)
+
+#Data types
+a=100
+b=20
+print(a+b)
+print()
+
+#Type Conversion
+a=100
+print(float(a))#convert int to float
+
+
+a=15.2233364
+print(int(a))#convert float to int
+print()
+
+d=float(input("Enter the Something:"))
+print(d)
+print()
+
+#commentlines
+print("#this is a single line comment.")
+"""this is comment line example """
+print("Multi comments are declared by using single,double or triple quote also")
